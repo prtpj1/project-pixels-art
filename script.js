ff0ec7942044b3305959b1fadf5c8c062836db5a
Paulo Porto <prtpj1@gmail.com>
2021-12-09 16:32:50 -0300

    Req 2 & 3: Done

---
+++ script.js
@@ -0,0 +1,5 @@
+let colors = document.getElementsByClassName('color');
+colors[0].style.backgroundColor = 'black';
+colors[1].style.backgroundColor = 'red';
+colors[2].style.backgroundColor = 'blue';
+colors[3].style.backgroundColor = 'green';

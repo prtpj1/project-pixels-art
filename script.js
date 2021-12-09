89353c883c8da4aa8e97f24d2f71153792d9aecc
Paulo Porto <prtpj1@gmail.com>
2021-12-09 19:40:00 -0300

    stuck on Req 4

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+window.onload = () => {
+      
+};
+
 let colors = document.getElementsByClassName('color');
+let pixels = document.getElementsByClassName('pixel');
+let pBoard = document.getElementById('pixel-board');
+
 colors[0].style.backgroundColor = 'black';
 colors[1].style.backgroundColor = 'red';
 colors[2].style.backgroundColor = 'blue';
 colors[3].style.backgroundColor = 'green';
+

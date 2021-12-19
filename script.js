69dd4ccf1051d188b9355bb4838a584261b8bc22
Paulo Porto <prtpj1@gmail.com>
2021-12-19 19:32:09 -0300

    Req 8 - Done

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function pickColor(event) {
   let colorSelected = document.querySelector('.selected');
   colorSelected.classList.remove('selected');
   event.target.classList.add('selected');
-  
 }
 
 colors[0].addEventListener('click', pickColor);
@@ -23,31 +22,17 @@ colors[3].addEventListener('click', pickColor);
 
 function createGrid(size) {
   pBoard.style.setProperty('--size', size);
-  for (let i = 0; i < size * size; i+=1) {
+  let selectedPixel = document.getElementsByClassName('color selected');
+  for (let i = 0; i < size * size; i += 1) {
     let divPixel = document.createElement('div');
     divPixel.classList.add('pixel');
-    //divPixel.style.height = '40px';
+
+    divPixel.addEventListener('click', function (event) {
+      let color = selectedPixel[0].style.backgroundColor;
+      event.target.style.backgroundColor = color;
+    });
+
     pBoard.appendChild(divPixel);
   }
 }
-
 createGrid(5);
-
-
-
-/* let colorPixel = document.querySelector('pixelColored');
-let line = '';
-
-function createBox(className) {
-    let box = document.createElement("div");
-    box.className = className;
-    return box;
-  }
-  
-for(let i = 0; i < pixels.length; i+=1) {
-    line = line + colorPixel;
-    
-}
-
-document.querySelector('pixelColored')
- */

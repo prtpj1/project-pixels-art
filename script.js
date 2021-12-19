63e0a3ab636ac965429878edfa06ce39b639878d
Paulo Porto <prtpj1@gmail.com>
2021-12-19 15:24:55 -0300

    Req 4 - Done

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onload = () => {
-      
-};
-
 let colors = document.getElementsByClassName('color');
 let pixels = document.getElementsByClassName('pixel');
 let pBoard = document.getElementById('pixel-board');
@@ -11,3 +7,32 @@ colors[1].style.backgroundColor = 'red';
 colors[2].style.backgroundColor = 'blue';
 colors[3].style.backgroundColor = 'green';
 
+function createGrid(size) {
+  pBoard.style.setProperty('--size', size);
+  for (let i = 0; i < size * size; i+=1) {
+    let divPixel = document.createElement('div');
+    divPixel.classList.add('pixel');
+    pBoard.appendChild(divPixel);
+  }
+}
+
+createGrid(5);
+
+
+
+/* let colorPixel = document.querySelector('pixelColored');
+let line = '';
+
+function createBox(className) {
+    let box = document.createElement("div");
+    box.className = className;
+    return box;
+  }
+  
+for(let i = 0; i < pixels.length; i+=1) {
+    line = line + colorPixel;
+    
+}
+
+document.querySelector('pixelColored')
+ */
